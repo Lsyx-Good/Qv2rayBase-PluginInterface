61fd7ad84f35ee5b0bea90f93513595c0dedc6e2
Moody <76251897+moodyhunter@users.noreply.github.com>
2021-07-31 16:26:27 +0800

    + support creating static plugins

--- cmake/QvPluginInterfaceMacros.cmake
+++ cmake/QvPluginInterfaceMacros.cmake
@@ -1,6 +1,7 @@
 # So high, so modern, so "cmake_path()"
 cmake_minimum_required(VERSION 3.20.0)
 
+option(QV2RAY_STATIC_PLUGINS "Create Static Plugins")
 set(CMAKE_INCLUDE_CURRENT_DIR ON)
 
 function(qv2ray_add_plugin_moc_sources TARGET)
@@ -21,13 +22,14 @@ function(qv2ray_add_plugin_gui_sources TARGET)
     target_sources(${TARGET} PRIVATE ${QvPluginInterface_Prefix}/QvPlugin/Gui/QvGUIPluginInterface.hpp)
 endfunction()
 
-function(qv2ray_configure_plugin TARGET_NAME)
-    set(options GUI Quick Widgets NO_INSTALL NO_RPATH HTTP_TO_SOCKS)
+function(qv2ray_add_plugin TARGET_NAME)
+    set(options GUI Quick Widgets NO_INSTALL NO_RPATH HTTP_TO_SOCKS STATIC)
     set(oneValueArgs
         INSTALL_PREFIX_LINUX
         INSTALL_PREFIX_WINDOWS
         INSTALL_PREFIX_MACOS
-        INSTALL_PREFIX_ANDROID)
+        INSTALL_PREFIX_ANDROID
+        CLASS_NAME)
     set(multiValueArgs
         EXTRA_DEPENDENCY_DIRS_WINDOWS)
     cmake_parse_arguments(QVPLUGIN "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
@@ -76,6 +78,15 @@ function(qv2ray_configure_plugin TARGET_NAME)
     if(DEFINED QVPLUGIN_INSTALL_PREFIX_ANDROID)
         message("INSTALL_PREFIX_ANDROID is not used")
     endif()
+
+    message("SHIT: ${QV2RAY_STATIC_PLUGINS}")
+
+    if(${QVPLUGIN_STATIC} OR ${QV2RAY_STATIC_PLUGINS})
+        set(QVPLUGIN_STATIC ON)
+        if((NOT DEFINED QVPLUGIN_CLASS_NAME) OR (QVPLUGIN_CLASS_NAME STREQUAL ""))
+            message(FATAL_ERROR "A static plugin must provide its main plugin class name.")
+        endif()
+    endif()
     # ====================================== END PARSING ARGUMENTS
 
     if(NOT QvPluginInterface_UseAsLib)
@@ -84,8 +95,32 @@ function(qv2ray_configure_plugin TARGET_NAME)
         get_target_property(QvPluginInterface_Prefix Qv2ray::QvPluginInterface INTERFACE_INCLUDE_DIRECTORIES)
     endif()
 
+    if(QVPLUGIN_STATIC)
+        add_library(${TARGET_NAME} STATIC)
+        target_compile_definitions(${TARGET_NAME} PRIVATE "QT_STATICPLUGIN=1")
+        message(STATUS "Generating static plugin importing source code for ${TARGET_NAME}")
+
+        get_target_property(OUT ${TARGET_NAME} BINARY_DIR)
+        set(IMPORT_SRC "${OUT}/${TARGET_NAME}_qv2ray_static_plugin_import.cpp")
+
+        # Write the file header
+        file(WRITE ${IMPORT_SRC} [[
+// Qv2ray Static Plugin Import File
+// File Generated via CMake script during build time.
+// Please rerun CMake to update this file, this file will be overwrite at each CMake run
+#include <QtPlugin>
+]]
+            )
+        file(APPEND ${IMPORT_SRC} "Q_IMPORT_PLUGIN(${QVPLUGIN_CLASS_NAME});")
+        message("Generated at: ${IMPORT_SRC}")
+        target_sources(${TARGET_NAME} INTERFACE ${IMPORT_SRC})
+        set_target_properties(${TARGET_NAME} PROPERTIES CXX_VISIBILITY_PRESET hidden)
+    else()
+        add_library(${TARGET_NAME} SHARED)
+        qv2ray_add_plugin_moc_sources(${TARGET_NAME})
+    endif()
+
     set_target_properties(${TARGET_NAME} PROPERTIES AUTOMOC ON)
-    qv2ray_add_plugin_moc_sources(${TARGET_NAME})
     set_property(TARGET ${TARGET_NAME} APPEND PROPERTY AUTOMOC_MACRO_NAMES "QV2RAY_PLUGIN")
 
     if(CMAKE_CXX_COMPILER_ID EQUAL Clang OR CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
@@ -132,31 +167,31 @@ function(qv2ray_configure_plugin TARGET_NAME)
         elseif(WIN32)
             install(TARGETS ${TARGET_NAME} RUNTIME DESTINATION ${QVPLUGIN_INSTALL_PREFIX_WINDOWS})
             install(CODE "
-    set(EXTRA_DIRS \"${QVPLUGIN_EXTRA_DEPENDENCY_DIRS_WINDOWS}\")
-    list(APPEND EXTRA_DIRS \"$<TARGET_PROPERTY:${TARGET_NAME},BINARY_DIR>\")
-    set(PLUGIN_INSTALL_PREFIX \"${CMAKE_INSTALL_PREFIX}/${QVPLUGIN_INSTALL_PREFIX_WINDOWS}/libs\")
-    set(TARGET_NAME ${TARGET_NAME})
-    set(TARGET_FILE \"$<TARGET_FILE:${TARGET_NAME}>\")
-    ")
+set(EXTRA_DIRS \"${QVPLUGIN_EXTRA_DEPENDENCY_DIRS_WINDOWS}\")
+list(APPEND EXTRA_DIRS \"$<TARGET_PROPERTY:${TARGET_NAME},BINARY_DIR>\")
+set(PLUGIN_INSTALL_PREFIX \"${CMAKE_INSTALL_PREFIX}/${QVPLUGIN_INSTALL_PREFIX_WINDOWS}/libs\")
+set(TARGET_NAME ${TARGET_NAME})
+set(TARGET_FILE \"$<TARGET_FILE:${TARGET_NAME}>\")
+")
 
-install(CODE [[
-    file(GET_RUNTIME_DEPENDENCIES
-        LIBRARIES ${TARGET_FILE}
-        RESOLVED_DEPENDENCIES_VAR "dependencies"
-        UNRESOLVED_DEPENDENCIES_VAR "un_depenendcies_unused"
-        DIRECTORIES ${EXTRA_DIRS}
-        )
-    foreach(dll ${dependencies})
-        foreach(dir ${EXTRA_DIRS})
-            cmake_path(IS_PREFIX dir "${dll}" NORMALIZE FOUND)
-            if(FOUND)
-                message(STATUS "${TARGET_NAME}: Found dependency: '${dll}'.")
-                file(COPY ${dll} DESTINATION ${PLUGIN_INSTALL_PREFIX})
-                break()
-            endif()
-        endforeach()
+            install(CODE [[
+file(GET_RUNTIME_DEPENDENCIES
+    LIBRARIES ${TARGET_FILE}
+    RESOLVED_DEPENDENCIES_VAR "dependencies"
+    UNRESOLVED_DEPENDENCIES_VAR "un_depenendcies_unused"
+    DIRECTORIES ${EXTRA_DIRS}
+    )
+foreach(dll ${dependencies})
+    foreach(dir ${EXTRA_DIRS})
+        cmake_path(IS_PREFIX dir "${dll}" NORMALIZE FOUND)
+        if(FOUND)
+            message(STATUS "${TARGET_NAME}: Found dependency: '${dll}'.")
+            file(COPY ${dll} DESTINATION ${PLUGIN_INSTALL_PREFIX})
+            break()
+        endif()
     endforeach()
-    ]])
+endforeach()
+]])
         elseif(APPLE)
             install(TARGETS ${TARGET_NAME} LIBRARY DESTINATION ${QVPLUGIN_INSTALL_PREFIX_MACOS})
         elseif(ANDROID)
@@ -171,22 +206,20 @@ install(CODE [[
         endif()
     endif()
 
-    message(STATUS "Qv2ray Plugin: ${TARGET_NAME}")
     message(STATUS "==========================================================")
-    message(STATUS "-- Components")
+    message(STATUS "Qv2ray Plugin ${TARGET_NAME}")
+    message(STATUS "        Static: ${QVPLUGIN_STATIC}")
     message(STATUS "      QWidgets: ${QVPLUGIN_Widgets}")
     message(STATUS "       QtQuick: ${QVPLUGIN_Quick}")
     message(STATUS "         QtGui: ${QVPLUGIN_GUI}")
     message(STATUS "    HTTP2SOCKS: ${QVPLUGIN_HTTP_TO_SOCKS}")
-    message(STATUS "-- Installation Options")
     message(STATUS "     NoInstall: ${QVPLUGIN_NO_INSTALL}")
     message(STATUS "       NoRPath: ${QVPLUGIN_NO_RPATH}")
-    message(STATUS "-- Installation Prefixes")
-    message(STATUS "        Global: ${CMAKE_INSTALL_PREFIX}")
-    message(STATUS "         Linux: ${QVPLUGIN_INSTALL_PREFIX_LINUX}")
-    message(STATUS "         macOS: ${QVPLUGIN_INSTALL_PREFIX_MACOS}")
-    message(STATUS "       Windows: ${QVPLUGIN_INSTALL_PREFIX_WINDOWS}")
-    message(STATUS "       Android: ${QVPLUGIN_INSTALL_PREFIX_ANDROID}")
+    message(STATUS " Global Prefix: ${CMAKE_INSTALL_PREFIX}")
+    message(STATUS "  Linux Prefix: ${QVPLUGIN_INSTALL_PREFIX_LINUX}")
+    message(STATUS "  macOS Prefix: ${QVPLUGIN_INSTALL_PREFIX_MACOS}")
+    message(STATUS "Windows Prefix: ${QVPLUGIN_INSTALL_PREFIX_WINDOWS}")
+    message(STATUS "Android Prefix: ${QVPLUGIN_INSTALL_PREFIX_ANDROID}")
     message(STATUS "==========================================================")
     message("")
 endfunction()
